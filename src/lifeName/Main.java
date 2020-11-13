1ba4ed89d77169c9d5cc06cdcd50e8e122ed2b75
Administrator <Administrator@N73617>
2020-11-13 20:40:43 +0800

    commit

--- src/lifeName/Main.java
+++ src/lifeName/Main.java
@@ -1,11 +1,22 @@
 package lifeName;
 
+import java.io.File;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		lifeName lifeName= new lifeName();
-		lifeName.creat();
-	}
-
+//		lifeName lifeName= new lifeName();
+//		lifeName.creat();
+//		
+//		 File file = new File("D:/course.txt");
+//	     System.out.println(lifeName.txt_String(file));
+		String a="love23next234csdn3423javaeye";
+		String regEx="[^0-9]";  
+		Pattern p = Pattern.compile(regEx);  
+		Matcher m = p.matcher(a);  
+		System.out.println( m.replaceAll(" ").trim());
+		}
 }
--- src/lifeName/lifeName.java
+++ src/lifeName/lifeName.java
@@ -1,8 +1,15 @@
 package lifeName;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class lifeName {
+
+	static int num1;
+	static int age;
+	static int[][] chess,chess_copy;
 	
 	public void creat() {
 		Scanner cin = new Scanner(System.in);
@@ -10,6 +17,7 @@ public class lifeName {
 		System.out.println("输入棋盘大小");
 		num1 = cin.nextInt();
 		int[][] chess = new int[num1][num1];
+		int[][] chess_copy = new int[num1][num1];
 		System.out.println("输入代数");
 		age = cin.nextInt();
 		System.out.println("输入活细胞个数");
@@ -20,39 +28,34 @@ public class lifeName {
 			chess[x][y] = 1;
 		}
 		
-		for(int j=0; j<num1; j++) {
-			for(int k=0; k<num1; k++) {
-				System.out.print(chess[j][k]);
-			}
-			System.out.println();
-		}
-		
 		System.out.println();
 		
 		for(int i=0; i<age; i++) {
-			for(int j=1; j<num1-1; j++) {
+			for(int j=0; j<num1; j++) {
 				int number = 0;
-				for(int k=1; k<num1-1; k++) {
-						if(chess[j-1][k-1] == 1)number++;
-						if(chess[j-1][k] == 1)number++;
-						if(chess[j-1][k+1] == 1)number++;
-						if(chess[j][k-1] == 1)number++;
-						if(chess[j][k+1] == 1)number++;
-						if(chess[j+1][k-1] == 1)number++;
-						if(chess[j+1][k] == 1)number++;
-						if(chess[j+1][k+1] == 1)number++;
+				for(int k=0; k<num1; k++) {
+						if(j-1 > -1 && k-1> -1)if(chess[j-1][k-1] == 1)number++;
+						if(j-1 > -1)if(chess[j-1][k] == 1)number++;
+						if(j-1 > -1 && k+1 < num1)if(chess[j-1][k+1] == 1)number++;
+						if(k-1> -1)if(chess[j][k-1] == 1)number++;
+						if(k+1 < num1)if(chess[j][k+1] == 1)number++;
+						if(k-1> -1 && j+1 < num1)if(chess[j+1][k-1] == 1)number++;
+						if(j+1 < num1)if(chess[j+1][k] == 1)number++;
+						if(j+1 < num1 && k+1 < num1)if(chess[j+1][k+1] == 1)number++;
 						
-						if(number == 3)chess[j][k] = 1;
-						else if(number == 2)chess[j][k] = chess[j][k];
-						else chess[j][k] = 0;
+						if(number == 3)chess_copy[j][k] = 1;
+						else if(number == 2)chess_copy[j][k] = chess[j][k];
+						else chess_copy[j][k] = 0;
 						
 						number = 0;
 				}
 			}
 			
+			chess = chess_copy;
+			
 			for(int j=0; j<num1; j++) {
 				for(int k=0; k<num1; k++) {
-					System.out.print(chess[j][k]);
+					if(chess[j][k] == 1)System.out.println(j+","+k);
 				}
 				System.out.println();
 			}
@@ -61,5 +64,23 @@ public class lifeName {
 		}
 	}
 	
-	
+	 public static String txt_String(File file){
+	        StringBuilder result = new StringBuilder();
+	        int count = 1;
+	        try{
+	            BufferedReader br = new BufferedReader(new FileReader(file));//构造一个BufferedReader类来读取文件
+	            String s = null;
+	            while((s = br.readLine())!=null){//使用readLine方法，一次读一行
+	                if(count == 1)num1 = Integer.parseInt(s);
+	                else if (count == 2)age = Integer.parseInt(s);
+	                else {
+						
+					}
+	            }
+	            br.close();    
+	        }catch(Exception e){
+	            e.printStackTrace();
+	        }
+	        return result.toString();
+	    }
 }
